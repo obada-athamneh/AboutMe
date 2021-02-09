645498a69f83d7528e5fdb35e0c83af21580fc8c
NOOR OMARI <nooromari847@gmail.com>
2021-02-09 21:28:05 +0200

    comment console

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ let score=0;
 let ansAge=prompt('Am I 30 years old?','yes or no');
 ansAge=ansAge.toLowerCase();
 if (ansAge ==='no' || ansAge ==='n') {
-  console.log('This is right');
+  //console.log('This is right');
   alert('This is right');
   score=score+1;
 } else if (ansAge ==='yes' || ansAge ==='y') {
-  console.log('No, not really');
+  //console.log('No, not really');
   alert('No, not really');
 } else{
   alert(`Hello ${userName}`);
@@ -18,11 +18,11 @@ if (ansAge ==='no' || ansAge ==='n') {
 let ansCity=prompt('Am I from Irbid?','yes or no');
 ansCity=ansCity.toLowerCase();
 if (ansCity ==='yes' || ansCity ==='y') {
-  console.log('This is right');
+  //console.log('This is right');
   alert('This is right');
   score=score+1;
 } else if (ansCity ==='no' || ansCity ==='n') {
-  console.log('No, not really');
+  //console.log('No, not really');
   alert('No, not really');
 } else{
   alert(`Hello ${userName}`);
@@ -31,11 +31,11 @@ if (ansCity ==='yes' || ansCity ==='y') {
 let ansBcountry=prompt('Am I born in Jordan?','yes or no');
 ansBcountry=ansBcountry.toLowerCase();
 if(ansBcountry ==='no' || ansBcountry ==='n') {
-  console.log('This is right');
+  //console.log('This is right');
   alert('This is right');
   score=score+1;
 } else if(ansBcountry ==='yes' || ansBcountry ==='y'){
-  console.log('No, not really');
+  //console.log('No, not really');
   alert('No, not really');
 } else{
   alert(`Hello ${userName}`);
@@ -44,11 +44,11 @@ if(ansBcountry ==='no' || ansBcountry ==='n') {
 let ansColor=prompt('Is Blue my favorite color?');
 ansColor=ansColor.toUpperCase();
 if (ansColor ==='YES' || ansColor==='Y') {
-  console.log('This is right');
+  //console.log('This is right');
   alert('This is right');
   score=score+1;
 } else if (ansColor ==='NO' || ansColor==='N'){
-  console.log('No, not really');
+  //console.log('No, not really');
   alert('No, not really');
 } else{
   alert(`Hello ${userName}`);
@@ -57,11 +57,11 @@ if (ansColor ==='YES' || ansColor==='Y') {
 let ansGrade=prompt('Did I graduate with weak grade from university?');
 ansGrade=ansGrade.toLowerCase();
 if (ansGrade ==='no' || ansGrade ==='n') {
-  console.log('This is right');
+  //console.log('This is right');
   alert('This is right');
   score=score+1;
 } else if (ansGrade ==='yes' || ansGrade ==='y'){
-  console.log('No, not really');
+  //console.log('No, not really');
   alert('No, not really');
 } else{
   alert(`Hello ${userName}`);
@@ -71,16 +71,16 @@ let myBirth=7;
 let ansBirth=prompt('What is my birthday month?','enter a number');
 for (let i = 0; i < 3; i++) {
   if (myBirth===parseInt(ansBirth)) {
-    console.log('This is right');
+    //console.log('This is right');
     alert('This is right');
     score=score+1;
     break;
   } else if (myBirth < parseInt(ansBirth)){
     ansBirth=prompt('too high');
-    console.log('too high');
+    //console.log('too high');
   } else if (myBirth > parseInt(ansBirth)) {
     ansBirth=prompt('too low');
-    console.log('too low');
+    //console.log('too low');
   }
   if (i===2 && (myBirth!==parseInt(ansBirth))) {
     alert(`Incorrect, the right answer is ${myBirth}`);
@@ -89,23 +89,23 @@ for (let i = 0; i < 3; i++) {
 let y=false;
 let myFilms=['animation','action','science fiction','comedy'];
 let ansFilms=prompt('What is my favorite movies type?');
-console.log('start for');
+//console.log('start for');
 for (let j = 0; j < 5; j++) {
-  console.log(j);
+  //console.log(j);
   for (let i = 0; i < myFilms.length; i++) {
-    console.log(i);
+    //console.log(i);
     if(ansFilms.toLowerCase() === myFilms[i]){
       alert('This is right');
       score=score+1;
       y = true;
-      console.log('22');
+    //console.log('22');
     }
   }
   if (y===true) {
     break;
   }
   ansFilms=prompt('incorrect, try again');
-  console.log(5);
+  //console.log(5);
 }
 
 let k=0;
